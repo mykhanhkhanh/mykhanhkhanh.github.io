a5e2def501471ab84b667a886a300992515c27dc
My Khanh Nguyen <95483654+mykhanhkhanh@users.noreply.github.com>
2022-04-22 00:48:11 +0700

    Update script.js

--- script.js
+++ script.js
@@ -145,10 +145,7 @@ Function.prototype.bind = Function.prototype.bind || function (target) {
       39: 1, // Right
       40: 2, // Down
       37: 3, // Left
-      75: 0, // Vim up
-      76: 1, // Vim right
-      74: 2, // Vim down
-      72: 3, // Vim left
+
       87: 0, // W
       68: 1, // D
       83: 2, // S
